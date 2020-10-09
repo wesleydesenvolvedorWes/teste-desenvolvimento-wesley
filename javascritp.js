58e5230601106c04abf141fde25d04ef3d687226
WESLEY <71525476+wesleydesenvolvedorWes@users.noreply.github.com>
2020-10-08 21:00:05 -0300

    javascritp.js
    javascritp.js

---
+++ javascritp.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+	$('#nav-menu').click(function(){
+		$('ul.nav-list').addClass('open').slideToggle('200');
+	});
+});
+
+function Buscar(){
+	alert('evento funcionando')
+}
+
+function vermais(){
+	alert('evento funcionando')
+}
